6f9c06ab4499e52ac7399a73e21e85c1ca4957b6
lnewmanheggie <lnewmanheggie@gmail.com>
2021-02-03 07:34:13 -0600

    changed var to const

--- server.js
+++ server.js
@@ -1,13 +1,12 @@
-var express = require("express");
-var path = require("path");
+const express = require("express");
+const path = require("path");
 const fs = require("fs")
 
-var app = express();
-var PORT = 3000;
+const app = express();
+const PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 app.use(express.static('public'));
 
 app.get("/notes", function(req, res) {
